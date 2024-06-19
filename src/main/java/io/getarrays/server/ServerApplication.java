db8b356e06bc5a9eeeb86d06c66688c87dd0b846
JananJavdan <fatemeh.javdan@student.intecbrussel.be>
2024-06-19 15:52:04 +0200

    latste update

--- src/main/java/io/getarrays/server/ServerApplication.java
+++ src/main/java/io/getarrays/server/ServerApplication.java
@@ -1,15 +1,15 @@
 package io.getarrays.server;
 
+import io.getarrays.server.enumeration.Status;
 import io.getarrays.server.model.Server;
 import io.getarrays.server.repo.ServerRepo;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
@@ -44,7 +44,7 @@ public class ServerApplication {
 				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename")));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter();
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 
 }
--- src/main/java/io/getarrays/server/resource/ServerResource.java
+++ src/main/java/io/getarrays/server/resource/ServerResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
